56559aa9ad0d8124a5d805905bce5bbcb7ebbad4
robs898 <rmackenzie8@gmail.com>
2017-11-09 21:38:57 +0000

    set figsize

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
-def graph_Example(chartID = 'chart_ID', chart_type = 'line', chart_height = 500):
+def graph():
     db = sqlite3.connect('coinsdb')
     df = pd.read_sql_query('SELECT * FROM coins', db)
-    dataSet = pandas_highcharts.core.serialize(df, render_to='my-chart', output_type='json')
+    dataSet = pandas_highcharts.core.serialize(df, render_to='my-chart', output_type='json', title="Longs", ylim=[-0.25,2], figsize=(1000,1000))
     return render_template('index.html', chart=dataSet)
